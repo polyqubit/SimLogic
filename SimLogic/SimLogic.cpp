3f1f4915befbfdaf6739f110ca11620fc69581a1
polyqubit <polyumbrial@gmail.com>
2022-11-14 12:00:39 -0800

    not fixed

--- SimLogic/SimLogic.cpp
+++ SimLogic/SimLogic.cpp
@@ -1,11 +1,23 @@
 #include <iostream>
 #include <fstream>
 
+//interesting discovery: for vscode, local dir is not the .exe's but the .sln file
+
 int main()
 {
     std::ifstream ifs;
-    ifs.open("../../hi.txt");
-    if (ifs) {
-        std::cout << "Hello World!\n";
+    ifs.open("message.txt");
+    if (!ifs) {
+        std::cout << "Bye World!\n";
+        exit(-1);
     }
+    std::cout << "Hello World!\n";
+
+    std::string a;
+    std::string bob;
+    while (ifs>>a) //when reading from file, space and newline are both used as separators
+    {
+        bob.append(a);
+    }
+    std::cout << bob;
 }
