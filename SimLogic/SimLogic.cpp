25dc8dd71de9b7262ef9964a386ab71cd3337322
polyqubit <polyumbrial@gmail.com>
2022-11-27 22:11:15 -0800

    starting the lexing of program

--- SimLogic/SimLogic.cpp
+++ SimLogic/SimLogic.cpp
@@ -11,7 +11,7 @@
 //interesting discovery: for visual studio, local dir is not the .exe's but the .sln file
 
 std::string read(std::string, std::string);
-int evaluate_expr(std::string);
+void evaluate_expr(std::string);
 
 int main()
 {
@@ -47,44 +47,14 @@ std::string read(std::string in, std::string name)
     while (std::getline(ifs, a)) //when reading from file, space and newline are both used as separators
     {
         bob.append(a.append(" = "));
-        bob.append(std::to_string(evaluate_expr(a)));
+        //bob.append(std::to_string(evaluate_expr(a)));
         bob.append("\n");
     }
     ifs.close();
     return bob;
 }
 
-int evaluate_expr(std::string line)
+void evaluate_expr(std::string line)
 {
-    std::vector<std::string> tokens;
-    int* nums = new int[8192];
-    if (line.find("+") >= 0)
-    {
-        try
-        {
-            /*int num1 = std::stoi(line.substr(0, sym_position));
-            int num2 = std::stoi(line.substr(sym_position + 1));
-            return num1 + num2;*/
-            while (line.length()>0)
-            {
-                int sym_position = line.find("+");
-                int num1 = std::stoi(line.substr(0, sym_position));
-                int num2 = std::stoi(line.substr(sym_position + 1));
-            }
-        }
-        catch (std::exception& e)
-        {
-            std::cout << "Exception: " << e.what() << "\n";
-            return 0;
-        }
-    }
-    try
-    {
-        return std::stoi(line);
-    }
-    catch (std::exception& e)
-    {
-        std::cout << "Exception: " << e.what() << "\n";
-        return 0;
-    }
+
 }
